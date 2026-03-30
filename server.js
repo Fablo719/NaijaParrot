dd7652fc04e1614a7e1587bb3051f78cc6a5677f
fablo <fabinuoluwadarasimi8@gmail.com>
2026-03-30 08:02:09 -0700

    my message

--- server.js
+++ server.js
@@ -1,34 +1,52 @@
 const express = require("express");
 const app = express();
-const cors = require("cors");
-require("dotenv").config();
-
+const mongoose = require("mongoose");
+const cors = require('cors');
+const dotenv = require("dotenv");
+dotenv.config();
+app.set("view engine", 'ejs');
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "50Mb" }));
 app.use(cors());
-
 // Routes
+const UserRouter = require('./routers/user.routes');
+const PostRouter = require('./routers/post.routes');
+const ProfileRouter = require('./routers/profile.routes')
+
 const connectDB = require("./database/connectDB");
-const UserRouter = require("./routers/user.routes");
-const PostRouter = require("./routers/post.routes");
+app.use('/api/v1/users', UserRouter);
+app.use('/api/v1/posts', PostRouter);
+app.use('/api/v1', ProfileRouter);
 
-app.use("/api/v1/users", UserRouter);
-app.use("/api/v1/posts", PostRouter);
+// Database connection
+// const DATABASE_URL = process.env.DATABASE_URI || process.env.MONGODB_URL;
+// console.log("Attempting to connect to MongoDB...");
 
-// Cache DB connection
-let isConnected = false;
+// mongoose.connect(DATABASE_URL)
+//     .then(() => {
+//         console.log("✅ Database connected successfully");
+//     })
+//     .catch((error) => {
+//         console.log("❌ Failed to connect to DB");
+//         console.log("Error details:", error.message);
+//     });
 
-module.exports = async (req, res) => {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
+// Server
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
 
-    return app(req, res);
-  } catch (error) {
-    console.log("Server error:", error.message);
-    res.status(500).json({ message: "Internal Server Error" });
+app.listen(process.env.PORT, (err) => {
+  if (err) {
+    console.log("error starting server", err);
+  } else {
+    console.log(`server started successfully`);
   }
-};
+});
+
+module.exports=async(req, res)=>{
+  await connectDB()
+
+  return app(req, res)
+}
