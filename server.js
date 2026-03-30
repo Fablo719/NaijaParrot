213aea55a8ce6959eb63cab6e480c9869dcc932f
fablo <fabinuoluwadarasimi8@gmail.com>
2026-03-30 06:06:32 -0700

    your message

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -9,6 +7,7 @@ require("dotenv").config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
+
 // Routes
 const connectDB = require("./database/connectDB");
 const UserRouter = require("./routers/user.routes");
@@ -17,28 +16,19 @@ const PostRouter = require("./routers/post.routes");
 app.use("/api/v1/users", UserRouter);
 app.use("/api/v1/posts", PostRouter);
 
-// DB
-// const DATABASE_URL = process.env.DATABASE_URI || process.env.MONGODB_URI;
+// Cache DB connection
+let isConnected = false;
 
-// mongoose
-//   .connect(DATABASE_URL)
-//   .then(() => console.log("✅ DB connected"))
-//   .catch((err) => console.log("❌ DB error:", err.message));
+module.exports = async (req, res) => {
+  try {
+    if (!isConnected) {
+      await connectDB();
+      isConnected = true;
+    }
 
-// // Server
-// const PORT = process.env.PORT || 5008;
-// app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
-
-app.listen(process.env.PORT, (err) =>{
-  if (err) {
-    console.log("error starting server", err);
-  } else {
-    console.log(`server started successfully`);
+    return app(req, res);
+  } catch (error) {
+    console.log("Server error:", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
   }
-});
-
-module.exports=async(req, res)=>{
-  await connectDB()
-
-  return app(req, res)
-}
+};
